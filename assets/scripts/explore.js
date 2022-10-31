f1ba3455f3e066141a6f4a0510ff3b6b1c04aa58
Amaar <avallian@ucsd.edu>
2022-10-30 18:57:00 -0700

    updated explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,46 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  setInterval(image, 500);
+  var select = document.querySelector("#voice-select");
+  var voices = [];
+  speechSynthesis.addEventListener('voiceschanged', () => {
+    voices = speechSynthesis.getVoices();
+    for(let i=0;i<voices.length;i++){
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      select.appendChild(option);
+    }
+  });
+  var currentVoice = "";
+  select.addEventListener('change', (event) => {
+    currentVoice = select.selectedOptions[0].getAttribute('data-name');
+  });
+  const button = document.querySelector("#explore").querySelector("button");
+  button.addEventListener('click', (event) => {
+    var textarea = document.querySelector("#explore").querySelector("#text-to-speak");
+    let speak = new SpeechSynthesisUtterance(textarea.value);
+    for(let i=0;i<voices.length;i++){
+      if(voices[i].name==currentVoice)
+        speak.voice = voices[i];
+    }
+    speechSynthesis.speak(speak);
+  });
 }
+
+function image() {
+  let face =  document.querySelector("#explore").querySelector("img");
+  if(speechSynthesis.speaking)
+    face.src = "/assets/images/smiling-open.png";
+  else
+    face.src = "/assets/images/smiling.png";
+}
+
+/*
+what did you say to me you little B? I will have you know 
+that I am top of my class in the Navy Seals with high ranking
+abilities in fish eating and laying in the sun. I even got a 
+honorable medal in arf arf so you better watch out.
+*/ 
